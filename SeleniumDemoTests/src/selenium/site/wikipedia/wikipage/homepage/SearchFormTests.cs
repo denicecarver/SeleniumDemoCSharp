65218bd9c93a5eb7ec06a244fca6807d6cc450da
Denice Carver <denicecarver@hotmail.com>
2015-12-03 21:03:13 -0800

    If tests are dummied in, they must fail until written.

--- SeleniumDemoTests/src/selenium/site/wikipedia/wikipage/homepage/SearchFormTests.cs
+++ SeleniumDemoTests/src/selenium/site/wikipedia/wikipage/homepage/SearchFormTests.cs
@@ -44,120 +44,31 @@ namespace com.selenium.wikitest.wikipage.homepage.Tests
         [TestMethod]
         public void searchForExistingTerm()
         {
-
-            // Get text to search
-            //String expectedResult = HomePageText.getString("SeleniumPage.SeleniumTitle");
-
-            // Run search from home page and get result
-            //String actualResult = homePage.searchFor(expectedResult);
-
-            // Assert expected matches actual
-            //try {
-            //              assertTrue(CommonMethods.formatAssertMessage(expectedResult, actualResult),
-            //                      actualResult.contains(expectedResult));
-            //          } catch (AssertionError ae) {
-            //              homePage.getUniqueScreenshot(this.toString());
-            //              ae.printStackTrace();
-            //              throw (ae);
-            //          }
+            Assert.Fail();
         }
 
         [TestMethod]
         public void searchForRedirectTerm()
         {
-
-            //// Get redirect text
-            //String expectedResult = WikiPageText.getString("AnyPage.RedirectText");
-
-            //// Go to redirected page
-            //homePage.searchFor(expectedResult);
-
-            //// Get text from non-redirected page
-            //String actualResult = homePage.searchForRedirect(expectedResult);
-
-            //// Assert expected matches actual
-            //try
-            //{
-            //    assertTrue(CommonMethods.formatAssertMessage(expectedResult, actualResult),
-            //            actualResult.contains(expectedResult));
-            //}
-            //catch (AssertionError ae)
-            //{
-            //    homePage.getUniqueScreenshot(this.toString());
-            //    ae.printStackTrace();
-            //    throw (ae);
-            //}
-
+            Assert.Fail();
         }
 
         [TestMethod]
         public void searchForBlankTerm()
         {
-
-  //          // Set expected outcome
-  //          String expectedResult = HomePageText.getString("ErrorPage.EmptyStringSearchTitle");
-
-  //          // Run search and get result.
-  //          String actualResult = homePage.searchFor(HomePageText.getString("HomePage.EmptyString"));
-
-		//// Assert expected == actual
-		//try {
-  //              assertTrue(CommonMethods.formatAssertMessage(expectedResult, actualResult),
-  //                      actualResult.contains(expectedResult));
-  //          } catch (AssertionError ae) {
-  //              homePage.getUniqueScreenshot(this.toString());
-  //              ae.printStackTrace();
-  //              throw (ae);
-  //          }
+            Assert.Fail();
         }
-
-        /*
-         * Data-driven test of multi-bit character set 
-         */
 
         [TestMethod]
         public void searchForLeftCurlyBracket()
         {
-
-            // Set expected outcome
-  //          String expectedResult = HomePageText.getString("HomePage.OpenBraceSearch");
-
-  //          // Run search and get result
-  //          String actualResult = homePage.searchFor(expectedResult);
-		
-		//// Assert expected matches actual
-		//try {
-  //              assertTrue(CommonMethods.formatAssertMessage(expectedResult, actualResult),
-  //                      actualResult.startsWith(expectedResult));
-  //          } catch (AssertionError ae) {
-  //              homePage.getUniqueScreenshot(this.toString());
-  //              ae.printStackTrace();
-  //              throw (ae);
-  //          }
-
+            Assert.Fail();
         }
 
         [TestMethod]
         public void searchForOutOfBoundsTerm()
         {
-
-  //          // Set expected outcome
-  //          String expectedResult = HomePageText.getString("ErrorPage.SearchErrored");
-
-  //          // Assert fails - bug
-  //          homePage.searchFor(HomePageText.getString("ErrorPage.SearchOutOfBoundsString"));
-  //          String actualResult = homePage.getSearchResultDetails();
-		
-		//try {
-  //              assertTrue(CommonMethods.formatAssertMessage(expectedResult, actualResult),
-  //                      actualResult.contains(expectedResult));
-  //          } catch (AssertionError ae) {
-  //              homePage.getUniqueScreenshot(this.toString());
-  //              ae.printStackTrace();
-  //              throw (ae);
-  //          }
-
-
+            Assert.Fail();
         }
 
         [TestMethod]
@@ -168,6 +79,7 @@ namespace com.selenium.wikitest.wikipage.homepage.Tests
             // Click go button
             // Get URL at search result page
             // Compare with expected value
+            Assert.Fail();
         }
 
     }
